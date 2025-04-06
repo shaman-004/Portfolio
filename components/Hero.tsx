473745a20f88802aa9b5c0df5d4b3dc9084e0b31
shaman-004 <shamanbusiness2004@gmail.com>
2025-04-06 14:13:00 +0530

    initial portofilio template updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,12 +52,12 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Welcome to My Portfolio"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Shaman M, a Frontend Developer and UI/UX Designer based in India.
           </p>
 
           <a href="#about">
